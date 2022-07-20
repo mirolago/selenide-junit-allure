dcb0651ea054c60fc9cbbb296319c4c5b1cb10d2
Miroslav Lago <miroslav.lago@aston.sk>
2022-07-20 13:48:45 +0200

    parallel run

--- src/test/java/DarcaKrviTest.java
+++ src/test/java/DarcaKrviTest.java
@@ -1,3 +1,4 @@
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -9,7 +10,146 @@ import static com.codeborne.selenide.Selenide.*;
 public class DarcaKrviTest extends Base {
 
     @Test
+    @Description("Test case 01")
     public void testCase01() throws IOException {
+        open(testUrl);
+        attachScreenshot("Main screen");
+        clickPokracovat("Slovenčina", 1);
+        $("#vyska").setValue("180");
+        $("#vaha").setValue("180");
+        clickRadio("pohlavie_MUZ");
+        clickPokracovat("Pokračovať", 2);
+        clickRadio("otazka1_ANO");
+        clickRadio("otazka2_ANO");
+        clickRadio("otazka3_ANO");
+        clickRadio("otazka4_ANO");
+        clickRadio("otazka5_ANO");
+        clickRadio("otazka6_NIE");
+        clickRadio("otazka7_ANO");
+        clickRadio("otazka8_ANO");
+        clickPokracovat("Pokračovať", 3);
+        clickRadio("otazka9aNie");
+        clickRadio("otazka9bNie");
+        clickRadio("otazka9cNie");
+        clickRadio("otazka9dNie");
+        clickRadio("otazka9eNie");
+        clickRadio("otazka9fNie");
+        clickRadio("otazka9gNie");
+        clickRadio("otazka9hNie");
+        clickRadio("otazka9iNie");
+        clickRadio("otazka9jNie");
+        clickRadio("otazka9kNie");
+        clickRadio("otazka9lNie");
+        clickRadio("otazka9mNie");
+        clickRadio("otazka10Nie");
+        clickRadio("otazka11Nie");
+        clickRadio("otazka12Nie");
+        clickPokracovat("Pokračovať", 4);
+        clickRadio("otazka13aNie");
+        clickRadio("otazka13bNie");
+        clickRadio("otazka13cNie");
+        clickRadio("otazka14Nie");
+        clickRadio("otazka15Nie");
+        clickRadio("otazka16aNie");
+        clickRadio("otazka16bNie");
+        clickRadio("otazka16cNie");
+        clickRadio("otazka17Nie");
+        clickRadio("otazka18Nie");
+        clickRadio("otazka19Nie");
+        clickRadio("otazka20Nie");
+        clickRadio("otazka21Nie");
+        clickRadio("otazka22aNie");
+        clickRadio("otazka22bNie");
+        clickRadio("otazka22cNie");
+        clickRadio("otazka23Nie");
+        clickRadio("otazka24Nie");
+        clickPokracovat("Pokračovať", 5);
+        $("#miestoPodpisu").setValue("Lozorno");
+        $("#datumPodpisu").setValue("01012022");
+        clickRadio("teplota37Nie");
+        clickRadio("pobytZahranicieNie");
+        clickRadio("kontaktZahranicieNie");
+        clickRadio("priznakOchoreniaNie");
+        clickRadio("prekonalCovidNie");
+        $$("input.button").findBy(value("Pokračovať")).click();
+        sleep(2000);
+        switchTo().window(0);
+        $(byText("Ďakujeme, že ste sa rozhodli darovať krv")).shouldBe(visible);
+        attachScreenshot("Step 6");
+    }
+
+    @Test
+    @Description("Test case 02")
+    public void testCase02() throws IOException {
+        open(testUrl);
+        attachScreenshot("Main screen");
+        clickPokracovat("Slovenčina", 1);
+        $("#vyska").setValue("180");
+        $("#vaha").setValue("180");
+        clickRadio("pohlavie_MUZ");
+        clickPokracovat("Pokračovať", 2);
+        clickRadio("otazka1_ANO");
+        clickRadio("otazka2_ANO");
+        clickRadio("otazka3_ANO");
+        clickRadio("otazka4_ANO");
+        clickRadio("otazka5_ANO");
+        clickRadio("otazka6_NIE");
+        clickRadio("otazka7_ANO");
+        clickRadio("otazka8_ANO");
+        clickPokracovat("Pokračovať", 3);
+        clickRadio("otazka9aNie");
+        clickRadio("otazka9bNie");
+        clickRadio("otazka9cNie");
+        clickRadio("otazka9dNie");
+        clickRadio("otazka9eNie");
+        clickRadio("otazka9fNie");
+        clickRadio("otazka9gNie");
+        clickRadio("otazka9hNie");
+        clickRadio("otazka9iNie");
+        clickRadio("otazka9jNie");
+        clickRadio("otazka9kNie");
+        clickRadio("otazka9lNie");
+        clickRadio("otazka9mNie");
+        clickRadio("otazka10Nie");
+        clickRadio("otazka11Nie");
+        clickRadio("otazka12Nie");
+        clickPokracovat("Pokračovať", 4);
+        clickRadio("otazka13aNie");
+        clickRadio("otazka13bNie");
+        clickRadio("otazka13cNie");
+        clickRadio("otazka14Nie");
+        clickRadio("otazka15Nie");
+        clickRadio("otazka16aNie");
+        clickRadio("otazka16bNie");
+        clickRadio("otazka16cNie");
+        clickRadio("otazka17Nie");
+        clickRadio("otazka18Nie");
+        clickRadio("otazka19Nie");
+        clickRadio("otazka20Nie");
+        clickRadio("otazka21Nie");
+        clickRadio("otazka22aNie");
+        clickRadio("otazka22bNie");
+        clickRadio("otazka22cNie");
+        clickRadio("otazka23Nie");
+        clickRadio("otazka24Nie");
+        clickPokracovat("Pokračovať", 5);
+        $("#miestoPodpisu").setValue("Lozorno");
+        $("#datumPodpisu").setValue("01012022");
+        clickRadio("teplota37Nie");
+        clickRadio("pobytZahranicieNie");
+        clickRadio("kontaktZahranicieNie");
+        clickRadio("priznakOchoreniaNie");
+        clickRadio("prekonalCovidNie");
+        $$("input.button").findBy(value("Pokračovať")).click();
+        sleep(2000);
+        switchTo().window(0);
+        $(byText("Ďakujeme, že ste sa rozhodli darovať krv")).shouldBe(visible);
+        attachScreenshot("Step 6");
+    }
+
+    @Test
+    @Description("Test case 03")
+    public void testCase03() throws IOException {
         open(testUrl);
         attachScreenshot("Main screen");
         clickPokracovat("Slovenčina", 1);
